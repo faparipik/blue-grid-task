e23254e55ded9970ddf04fd24a140bca694c7684
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2024-06-23 14:58:03 +0200

    removed unused import

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,4 +1,4 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, Request, Response } from "express";
 
 interface ResponseError extends Error {
   status?: number;
